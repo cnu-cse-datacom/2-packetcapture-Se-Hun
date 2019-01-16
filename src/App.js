b45a598e90137b6ff0991a72c892ecf2c79f5061
201502029 <tnehd158@gmail.com>
2019-01-17 00:54:59 +0900

    홈페이지 구성완료(세부사항은 진해안됨)

--- src/App.js
+++ src/App.js
@@ -9,35 +9,36 @@ class App extends Component {
           <title>강아지 가이드</title>
         </head>
         <body className="line">
-          <div className="head">
-            <div className="headlinelogo headline">
-              <img className="headicon headline"
+          <div className="headline">
+            <div className="headlinelogo headfactor">
+              <img className="headicon headfactor"
               src="https://image.flaticon.com/icons/svg/1196/1196308.svg"
               alt="dog icon"
-              title="강아지 가이드"
-              width="90"
-              height="60"/>
+              title="강아지 가이드"/>
             </div>
-            <div className="headlineexplanation headline">
-              <div className="header">강아지 가이드</div>
-              <div>dog guide</div>
+            <div className="headlineexplanation headfactor">
+              <p className="header">강아지 가이드</p>
+              <p>dog guide</p>
             </div>
-            <div className="headlinesearch headline">
-              <p>search</p>
+            <div className="headlinesearch headfactor">
+              <p className="height5">search</p>
             </div>
-            <div className="headlinelogin headline">
-              <p>login</p>
+            <div className="headlinelogin headfactor">
+              <p className="height5">login</p>
             </div>
-            <div className="headlinejoin headline">
-              <p>join</p>
+            <div className="headlinejoin headfactor">
+              <p className="height5">join</p>
+            </div>
+            <div className="headlineetc headfactor">
+              <p className="height5">etc</p>
             </div>
           </div>
           <div className="bodyline">   
-            <img
-            src="https://images.unsplash.com/photo-1546421845-6471bdcf3edf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-            alt="human and dog"
-            width="100%"
-            title="강아지 가이드"/>
+            <div className="maindisplay">
+              <h1>강아지 가이드</h1>
+              <p>강아지에 대한 정보를 알아보세요!<br />다양한 종류의 강아지들을 알려드립니다!</p>
+              <p>search</p>
+            </div>
           </div> 
         </body>
       </html>
